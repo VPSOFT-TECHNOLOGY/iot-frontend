a30680978c3bd0917e74938fb5b8ca55796e1739
victorchan <LE17081467@merida.tecnm.mx>
2022-01-13 19:17:12 -0600

    Second commit

--- src/components/Charts.jsx
+++ src/components/Charts.jsx
@@ -2,23 +2,23 @@ import { BarChart, CartesianGrid, XAxis, YAxis,Tooltip,Legend,Bar } from 'rechar
 
 const Charts = () => { 
     const data = [
-        {name: 'Lunes', Intruciones: 100, amt: 2400},
-        {name: 'Martes', Intruciones: 100, amt: 2400},
-        {name: 'Miercoles', Intruciones: 50, amt: 2400},
-        {name: 'Jueves', Intruciones: 10, amt: 2400},
-        {name: 'Viernes', Intruciones: 50, amt: 2400},
-        {name: 'Sabado', Intruciones: 300, amt: 2400},
-        {name: 'Domingo', Intruciones: 300, amt: 2400},
+        {name: 'Lunes', Intrusiones: 100, amt: 2400},
+        {name: 'Martes', Intrusiones: 100, amt: 2400},
+        {name: 'Miercoles', Intrusiones: 50, amt: 2400},
+        {name: 'Jueves', Intrusiones: 10, amt: 2400},
+        {name: 'Viernes', Intrusiones: 50, amt: 2400},
+        {name: 'Sabado', Intrusiones: 300, amt: 2400},
+        {name: 'Domingo', Intrusiones: 220, amt: 2400},
     ]
     return (
         <BarChart width={730} height={250} data={data}>
-            <CartesianGrid strokeDasharray="3 3" />
-            <XAxis dataKey="name" />
-            <YAxis />
-            <Tooltip />
-            <Legend />
-            <Bar dataKey="Intruciones" fill="#82ca9d" />
+          <CartesianGrid strokeDasharray="3 3" />
+          <XAxis dataKey="name" />
+          <YAxis />
+          <Tooltip />
+          <Legend />
+          <Bar dataKey="Intrusiones" fill="#82ca9d" />
         </BarChart>
-    )
+            )
 }
 export default Charts
